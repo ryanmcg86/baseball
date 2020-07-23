ca8e67f16e1d3eacc72897f4d5d3b3f4e0125a2c
Ryan <ryanmcg86@users.noreply.github.com>
2020-07-23 13:33:13 -0400

    Create mile_per_hour.py

---
+++ mile_per_hour.py
@@ -0,0 +1,72 @@
+#velo is in miles per hour
+
+from math import sin, cos, radians as r
+-------------------------------------------------------------------!
+#time until ball hits its apex
+#returns seconds
+def apexTime(velo, angle):
+	g = 22 #mph/s
+	return velo * sin(r(angle)) / g
+-------------------------------------------------------------------!
+#horizontal velocity
+#returns m/s
+def Vx(velo, angle):
+	return velo * cos(r(angle))
+-------------------------------------------------------------------!
+#virtical velocity at time t
+#returns m/s
+def Vy(velo, angle, t):
+	g = 22	#mph/s
+	return velo * sin(r(angle)) - g * t
+-------------------------------------------------------------------!
+#distance at time t
+#for distance at the apex, use apexTime(velo, angle) as t
+#for distance when the ball lands, use totalTime(velo, angle) as t
+#returns meters
+def x(velo, angle, t):
+	return velo * cos(r(angle)) * t
+-------------------------------------------------------------------!
+#height at time t
+#returns meters
+def y(velo, angle, t):
+	g = 22	#mph/s
+	return velo * sin(r(angle)) * t - (0.5) * g * t**2
+-------------------------------------------------------------------
+#the maximum height given a velocity and angle
+#returns meters
+def maxHeight(velo, angle):
+	return y(velo, angle, apexTime(velo, angle))
+-------------------------------------------------------------------!
+#total time until ball hits the ground
+#returns seconds
+def totalTime(velo, angle):
+	g = 22	#mph/s
+	return 2 * velo * sin(r(angle)) / g
+-------------------------------------------------------------------!
+#total distance travelled when ball hits the ground
+#returns meters
+def totalDistance(velo, angle):
+	return velo * cos(r(angle)) * totalTime(velo, angle)
+
+
+#test it out!
+velo, angle = 44.73, 25 #velocity = 20 m/s = 44.73 mph, angle = 25 degrees
+
+#Times
+apexTime(velo, angle)      #Output: 0.8592597658027947
+totalTime(velo, angle)     #Output: 1.7185195316055895
+
+#Velocity Components
+Vx(velo, angle)            #Output: 40.539147314149346
+Vy(velo, angle, 0.5)       #Output: 7.903714847661483
+Vy(velo, angle, 1)         #Output: -3.096285152338517
+
+#Distance
+x(velo, angle, 0.5)        #Output: 9.063077870366499
+x(velo, angle, 1)          #Output: 18.126155740732997
+totalDistance(velo, angle) #Output: 31.267120127305223
+
+#Height
+y(v, a, 0.5)               #Output: 3.0011826174069944
+y(v, a, 1)                 #Output: 3.5523652348139887
+maxHeight(velo, angle)     #Output: 3.6450243909536804
